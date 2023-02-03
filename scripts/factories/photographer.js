fb9c7e6c6461b7539c6aee86ac0cc1cd56d1056e
Dubar Jérémy <jeremy.dubar@decathlon.com>
2023-02-03 16:19:53 +0100

    feat(step2+3): Add photographers cards + css

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,18 +1,57 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 function photographerFactory(data) {
-    const { name, portrait, id, city, country, tagline } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
+
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.classList.add('card')
+        
+        const cardPicture = elementBuilder({
+            type: 'img',
+            role: 'img',
+            ariaLabel: `portrait de ${name}`,
+            alt: `portrait de ${name}`,
+            src: picture, 
+            className: ["card-img"]
+        });
+        // mettre un role sur cardTitle ?
+        const cardTitle = elementBuilder({
+            type: 'h2',
+            content: name,
+            ariaLabel: 'title of the card',
+            className: ["card-title"]
+        });
+        const cardLocation = elementBuilder({
+            type: 'p',
+            content: `${city}, ${country}`,
+            ariaLabel: `location of ${name}`,
+            className: ["card-location"]
+        });
+        const cardDescription = elementBuilder({
+            type: 'p',
+            content: tagline,
+            ariaLabel: `the job description of ${name}`,
+            className: ["card-description"]
+        });
+        const cardPrice = elementBuilder({
+            type: 'p',
+            content: `${price}€/jour`,
+            ariaLabel: `the price of ${name}`,
+            className: ["card-price"]
+        });
+
+        article.appendChild(cardPicture);
+        article.appendChild(cardTitle);
+        article.appendChild(cardLocation);
+        article.appendChild(cardDescription);
+        article.appendChild(cardPrice);
+
         return (article);
     }
+
     return { name, picture, id, city, country, tagline, getUserCardDOM }
 }
